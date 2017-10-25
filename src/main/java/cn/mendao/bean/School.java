9ad19c54615676aab1d5fad6482662964fc4b027
赵一凡 <674097325@qq.com>
2017-10-25 14:39:08 +0800

    修改

---
+++ src/main/java/cn/mendao/bean/School.java
@@ -0,0 +1,36 @@
+package cn.mendao.bean;
+
+/**
+ * Created by warden on 17/10/25.
+ */
+public class School {
+
+    private long sort;
+    private String code;
+    private String name;
+
+
+    public long getSort() {
+        return sort;
+    }
+
+    public void setSort(long sort) {
+        this.sort = sort;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/cn/mendao/controller/TestController.java
@@ -0,0 +1,34 @@
+package cn.mendao.controller;
+
+import cn.mendao.bean.School;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Scope("prototype")
+@Controller("testController")
+@RequestMapping(value = "/")
+public class TestController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @ResponseBody
+    @RequestMapping(value = "/test",method = RequestMethod.GET)
+    public Object clickView(HttpServletRequest request){
+        School school = new School();
+        school.setSort(1);
+        school.setCode("1001");
+        school.setName("清华大学");
+        return school;
+    }
+
+
+
+
+}
